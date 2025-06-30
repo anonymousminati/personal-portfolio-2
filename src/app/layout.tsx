a218f8ed8621ba9fb96daaf730856fce953d16b9
anonymousminati <prathameshmalode.2@gmail.com>
2025-06-30 21:18:43 +0530

    push

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,9 +5,60 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Modern Portfolio - John Doe',
-  description: 'Full Stack Developer & UI/UX Designer passionate about creating exceptional digital experiences',
-}
+  title: 'Prathamesh Malode | Full Stack Developer',
+  description: 'Portfolio of Prathamesh Malode, a Full Stack Web Developer specializing in modern, scalable architectures.',
+  keywords: [
+    'Prathamesh Malode',
+    'Full Stack Developer',
+    'Web Developer',
+    'Portfolio',
+    'React Developer',
+    'Next.js Developer',
+    'JavaScript Developer',
+    'Frontend Developer',
+    'Backend Developer',
+    'Software Engineer',
+    'Full Stack Web Developer',
+    'Portfolio Website',
+    'Web Development',
+    'Software Development',
+    'Programming',
+    'Software Engineer Portfolio',
+    'Web Development Portfolio',
+    'React.js Portfolio',
+    'Next.js Portfolio',  
+    'JavaScript Portfolio',
+    'Frontend Portfolio',
+    'Backend Portfolio',
+    'Software Engineer Projects',
+    'Web Development Projects',
+    'React.js Projects',
+    'Next.js Projects',
+    'JavaScript Projects',
+    'Frontend Projects',
+    'Backend Projects',
+    'Software Engineer Skills',
+    'Web Development Skills',
+    'React.js Skills',
+    'Next.js Skills',
+    'JavaScript Skills',
+    'Frontend Skills',
+    'Backend Skills',
+  ],
+  authors: [
+    {
+      name: 'Prathamesh Malode',
+      url: 'https://anonymousminati.github.io/personal-portfolio-2/',
+    },
+  ],
+  creator: 'Prathamesh Malode',
+  openGraph: {
+    title: 'Prathamesh Malode | Full Stack Developer',
+    description: 'Portfolio of Prathamesh Malode, a Full Stack Web Developer specializing in modern, scalable architectures.',
+    url: 'https://anonymousminati.github.io/personal-portfolio-2/',
+    siteName: 'Prathamesh Malode'
+    }
+  }
 
 export default function RootLayout({
   children,
@@ -15,7 +66,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en"  suppressHydrationWarning>
       <body className={inter.className}>{children}</body>
     </html>
   )
